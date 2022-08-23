494059ba71d3970fc82c83b3201c80fc4408627f
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2022-08-24 03:21:24 +0800

    docs: Add API Documentation to DeleteSubjectByIdEndpoint

---
+++ NoobNoob.eWAN.WebApis/Summaries/DeleteSubjectByIdSummary.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using FastEndpoints;
+using NoobNoob.eWAN.Application.Contracts.Responses;
+using NoobNoob.eWAN.WebApis.Endpoints;
+
+namespace NoobNoob.eWAN.WebApis.Summaries;
+
+public class DeleteSubjectByIdSummary : Summary<DeleteSubjectByIdEndpoint>
+{
+    public DeleteSubjectByIdSummary()
+    {
+        Summary = "Deletes a Subject";
+        Description = "Deletes a Subject in the System";
+        Response((int)HttpStatusCode.OK, "Subject was deleted");
+        Response<ValidationFailureResponse>((int)HttpStatusCode.BadRequest, "The request did not pass validation checks");
+    }
+}
