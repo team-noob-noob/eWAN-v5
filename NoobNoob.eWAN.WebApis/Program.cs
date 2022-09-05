b0a1fc9f86ebffccbc007a78ab99ac6f0f776cab
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2022-09-06 02:06:10 +0800

    feat: Configure webapis to accept oauth2 authn

--- NoobNoob.eWAN.WebApis/Program.cs
+++ NoobNoob.eWAN.WebApis/Program.cs
@@ -8,18 +8,67 @@ using NoobNoob.eWAN.Infrastructure.Data.EfCoreCosmos;
 using NoobNoob.eWAN.Infrastructure.Data.EfCoreCosmos.Repositories;
 using NoobNoob.eWAN.Infrastructure.Data.EfCoreCosmos.Services;
 using NoobNoob.eWAN.WebApis.Middlewares;
+using NSwag;
+using NSwag.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddFastEndpoints();
-builder.Services.AddSwaggerDoc();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();        
+    });
+});
+builder.Services.AddSwaggerDoc(options =>
+{
+    options.AddAuth("OAuth2", new OpenApiSecurityScheme()
+    {
+        Type = OpenApiSecuritySchemeType.OAuth2,
+ 
+        Flows = new OpenApiOAuthFlows()
+        {
+            AuthorizationCode = new OpenApiOAuthFlow()
+            {
+#if DEBUG
+                AuthorizationUrl = "https://localhost:5002/connect/authorize",
+                TokenUrl = "https://localhost:5002/connect/token",
+#else
+                AuthorizationUrl = Environment.GetEnvironmentVariable("IDENTITY_AUTHORIZATION_URL"),
+                TokenUrl = Environment.GetEnvironmentVariable("IDENTITY_TOKEN_URL"),
+#endif
+                
+                Scopes = new Dictionary<string, string>()
+                {
+                    {"APIS", "Access the WebApis"}
+                }
+            }
+        }
+    });
+}, addJWTBearerAuth: false);
 builder.Services.AddDbContext<EwanCosmosDbContext>();
 builder.Services.AddSingleton<IUnitOfWork, UnitOfWork>();
 builder.Services.AddSingleton<ISubjectRepository, SubjectRepository>();
 builder.Services.AddSingleton<ISubjectService, SubjectService>();
+builder.Services.AddAuthorization();
+builder.Services
+    .AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:5002";
+        options.Audience = "APIS";
+#if DEBUG
+        options.RequireHttpsMetadata = false;
+#endif
+    });
 
 var app = builder.Build();
-
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseMiddleware<ValidationExceptionMiddleware>();
 app.UseFastEndpoints(x =>
 {
@@ -32,7 +81,19 @@ app.UseFastEndpoints(x =>
     };
 });
 app.UseOpenApi();
-app.UseSwaggerUi3(s => s.ConfigureDefaults());
-
+app.UseSwaggerUi3(settings =>
+{
+    settings.OAuth2Client = new OAuth2ClientSettings()
+    {
+#if DEBUG
+        ClientId = "49C1A7E1-0C79-4A89-A3D6-A37998FB86B0",
+        ClientSecret = "49C1A7E1-0C79-4A89-A3D6-A37998FB86B0",
+#else
+        ClientId = Environment.GetEnvironmentVariable("IDENTITY_CLIENT_ID"),
+        ClientSecret = Environment.GetEnvironmentVariable("IDENTITY_CLIENT_SECRET"),
+#endif
+        AppName = "eWAN WebApis",
+    };
+});
 
 app.Run();
