8a3d0754bb9e3ba743198c5f7f362c5beab14935
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2023-01-16 00:34:55 +0000

    refactor: remove old identity server

--- NoobNoob.eWAN.IdentityServer/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace NoobNoob.eWAN.IdentityServer.Pages.Logout;
-
-public class LogoutOptions
-{
-    public static bool ShowLogoutPrompt = true;
-    public static bool AutomaticRedirectAfterSignOut = false;
-}
